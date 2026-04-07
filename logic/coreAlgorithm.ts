0c55afefd6284f4fb037f054fe4b3fb5653282f7
tomshalbar <tomshalbar@gmail.com>
2026-04-07 17:48:29 -0400

    add basic instructions to core
    Ensure the returned schedule is a parsable JSON string.

--- logic/coreAlgorithm.ts
+++ logic/coreAlgorithm.ts
@@ -2,6 +2,6 @@ export async function generateScheduleRecommendation(
   schedule: string,
   preferences: Record<string, string>,
 ) {
-  // change this to return the new schedule.
+  // change this to return the new schedule. **Make sure that it is reutrning a string that can be parsed into a JSON, just like the in schedule.**
   return schedule;
 }
